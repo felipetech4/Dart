4664d74933dc47e98fca1520fead883077f901ff
felipetech4 <97922422+felipetech4@users.noreply.github.com>
2025-04-10 22:20:55 -0300

    implemetei a funcao de listar todos e sair do programa no menu inicial

--- primeiro_app/bin/primeiro_app.dart
+++ primeiro_app/bin/primeiro_app.dart
@@ -51,6 +51,26 @@ void menu(int opcao){
       editarProduto(mapaProdutos, produtoAntigo, produtoNovo, valor);
       voltarMenu();
       break;
+
+      case 4 : 
+      stdout.write("Digite o nome do produto a ser excluído: ");
+      String produto = stdin.readLineSync()!;
+      excluirProduto(mapaProdutos, produto);
+      voltarMenu();
+      break;
+
+      case 5 :
+      listarProdutos(mapaProdutos);
+      voltarMenu();
+      break;
+
+      case 0 :
+      print("Pressione ENTER para sair do programa...");
+      stdin.readLineSync();
+
+      default :
+      print("Opção Inválida... Tente novamente!");
+      stdin.readLineSync();
     }
   }
 }
@@ -72,7 +92,9 @@ void editarProduto(Map<String , double> mapaProdutos, String produtoAntigo, Stri
 
 void excluirProduto(Map<String , double> mapaProdutos, String produto){
   mapaProdutos.remove(produto);
+  print("************************************************");
   print("O produto '$produto' foi excluído com sucesso!");
+  print("************************************************");
 }
 
 void detalharProduto(Map<String , double> mapaProdutos, String produtoEntrada){
@@ -88,14 +110,11 @@ void detalharProduto(Map<String , double> mapaProdutos, String produtoEntrada){
 }
 
 void listarProdutos(Map<String , double> mapaProdutos){
-  mapaProdutos.forEach((produto, valor){
-    print("---------------------------------");
     print("---------------------------------");
     print("Lista de Produtos");
+    mapaProdutos.forEach((produto, valor){
     print("---------------------------------");
-    print("---------------------------------");
-    print("Produto: $produto | Valor: R\$${valor.toStringAsFixed(2)}");
-    print("---------------------------------");
+    print("Descrição: $produto | Valor: R\$${valor.toStringAsFixed(2)}");
   });
 }
 
@@ -103,81 +122,3 @@ void voltarMenu(){
   print("Pressione ENTER para voltar ao menu inicial...");
   stdin.readLineSync();
 }
-
-
-/*
-import 'dart:io';
-
-void main() {
-  Map<String, double> mapaProdutos = {
-    "Farol de neblina gol G5": 109.90,
-    "Lâmpada de Lanterna Traseira Fusca 1969": 399.90,
-    "Seta Esquerda Siena 2007": 39.00
-  };
-
-  int opcao = -1;
-
-  while (opcao != 0) {
-    print("\n===== MENU =====");
-    print("1 - Cadastrar produto");
-    print("2 - Remover produto");
-    print("3 - Listar produtos");
-    print("0 - Sair");
-    stdout.write("Escolha uma opção: ");
-    opcao = int.tryParse(stdin.readLineSync()!) ?? -1;
-
-    switch (opcao) {
-      case 1:
-        stdout.write("Digite o nome do produto: ");
-        String produto = stdin.readLineSync()!;
-        stdout.write("Digite o valor do produto: ");
-        double valor = double.tryParse(stdin.readLineSync()!) ?? 0.0;
-        cadastrarProduto(mapaProdutos, produto, valor);
-        break;
-
-      case 2:
-        stdout.write("Digite o nome do produto a remover: ");
-        String produto = stdin.readLineSync()!;
-        removerProduto(mapaProdutos, produto);
-        break;
-
-      case 3:
-        listarProdutos(mapaProdutos);
-        break;
-
-      case 0:
-        print("Encerrando o programa...");
-        break;
-
-      default:
-        print("Opção inválida. Tente novamente.");
-    }
-  }
-}
-
-void cadastrarProduto(Map<String, double> mapaProdutos, String produto, double valor) {
-  mapaProdutos[produto] = valor;
-  print("Produto '$produto' cadastrado com sucesso!");
-}
-
-void removerProduto(Map<String, double> mapaProdutos, String produto) {
-  if (mapaProdutos.containsKey(produto)) {
-    mapaProdutos.remove(produto);
-    print("Produto '$produto' removido com sucesso!");
-  } else {
-    print("Produto '$produto' não encontrado.");
-  }
-}
-
-void listarProdutos(Map<String, double> mapaProdutos) {
-  if (mapaProdutos.isEmpty) {
-    print("Nenhum produto cadastrado.");
-  } else {
-    print("\n--- Produtos em Estoque ---");
-    mapaProdutos.forEach((produto, valor) {
-      print("Produto: $produto | Valor: R\$${valor.toStringAsFixed(2)}");
-    });
-  }
-}
-
-*/
