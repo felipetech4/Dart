1e106223db3519c81958070f50b08be404a7776b
felipetech4 <97922422+felipetech4@users.noreply.github.com>
2025-04-23 22:44:54 -0300

    criando subclasse para treinar heranca de classes

---
+++ orientacao_objetos/bin/carroEsportivo.dart
@@ -0,0 +1,37 @@
+import 'carro.dart';
+
+class CarroEsportivo extends Carro {
+  bool _modoTurbo = false;
+  //Construtor
+  CarroEsportivo(String marca, String modelo, int ano)
+    : super(marca, modelo, ano);
+  //Getter
+  bool get modoTurbo => _modoTurbo;
+  //Setter
+  set modoTurbo(bool estadoTurbo) {
+    if (!estadoTurbo) {
+      _modoTurbo = estadoTurbo;
+    }
+    if (estadoTurbo && ano >= 2015) {
+      _modoTurbo = estadoTurbo;
+    } else {
+      print("Carros anteriores a 2015 não suportam modo turbo.");
+    }
+  }
+
+  void ativarTurbo() {
+    modoTurbo = true;
+    print("Modo turbo ativado!");
+  }
+
+  void desativarTurbo() {
+    modoTurbo = false;
+    print("Modo turbo desativado...");
+  }
+
+  @override
+  void exibirDetalhes() {
+    super.exibirDetalhes();
+    print("Modo turbo: ${_modoTurbo ? "Ativado" : "Desativado"}");
+  }
+}
