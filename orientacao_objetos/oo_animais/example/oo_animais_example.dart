ab988cc4452fbf4f916952fa8e23d5cce88608e8
felipetech4 <97922422+felipetech4@users.noreply.github.com>
2025-04-23 23:53:57 -0300

    sunbindo projeto para treinar polimorfismo

---
+++ orientacao_objetos/oo_animais/example/oo_animais_example.dart
@@ -0,0 +1,6 @@
+import 'package:oo_animais/oo_animais.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ orientacao_objetos/oo_animais/test/oo_animais_test.dart
@@ -0,0 +1,16 @@
+import 'package:oo_animais/oo_animais.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
