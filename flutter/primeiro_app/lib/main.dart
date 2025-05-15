4ee9953f46eb487b3e448691d073f544e4a7a117
felipetech4 <97922422+felipetech4@users.noreply.github.com>
2025-05-14 23:32:11 -0300

    criando tela com varios widgets

--- flutter/primeiro_app/lib/main.dart
+++ flutter/primeiro_app/lib/main.dart
@@ -1,23 +1,45 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Primeirp App',
+      title: 'App_Simples',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Meu primeiro app'
-          ),
-        ),
+        appBar: AppBar(title: Text('Tela Principal')),
         body: Center(
-          child: Text(
-            'Olá Mundo!'
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.favorite, color: Colors.red, size: 80),
+              SizedBox(height: 20),
+              Text('Você curtiu isso!', style: TextStyle(fontSize: 24)),
+              SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      print('Clicou!');
+                    },
+                    child: Text('Clicar'),
+                  ),
+                  SizedBox(width: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('Compartilhou');
+                    },
+                    child: Text('Compartilhar'),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
