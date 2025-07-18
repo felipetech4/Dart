88faa7ca11d40d188d5417dedd11e6abc83ebb1e
felipetech4 <97922422+felipetech4@users.noreply.github.com>
2025-07-18 01:11:01 -0300

    corrigindo bug de debug por atalho devido ao nome do arquivo principal

--- flutter/controle_financeiro_app/test/widget_test.dart
+++ flutter/controle_financeiro_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:controle_financeiro_app/tela_inicial.dart';
+import 'package:controle_financeiro_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
